797dc6e81f63bf3a4a1480fbe002c5a10c97cf3e
JailsonSantos <jailson.ads007@gmail.com>
2022-11-17 15:33:13 -0300

    CSS-in-JS - Criando Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,14 @@
-import { Header } from '@components/Header';
-import { HeightLight } from '@components/Highlight';
-import { StyleSheet, Text, View } from 'react-native';
-
 import { Container } from './styles';
+import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
+import { HeightLight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <HeightLight title="Turmas" subtitle="Jogue com a sua Turma" />
+      <GroupCard title="Galera do IGNITE" />
     </Container>
   );
 }
