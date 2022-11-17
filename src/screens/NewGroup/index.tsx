2d946f7ab323d09daacca13879fed8eeef5ac747
JailsonSantos <jailson.ads007@gmail.com>
2022-11-17 17:05:22 -0300

    CSS-in-JS - Estilizando o Placeholder

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -17,7 +17,9 @@ export function NewGroup() {
           subtitle="Cria a turma para adicioanr as pessoas."
         />
 
-        <Input />
+        <Input
+          placeholder="Nome da turma"
+        />
 
         <Button title="Criar" style={{ marginTop: 20 }} />
 
