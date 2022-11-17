a65a977fac9b307261416db264d12e58f9d6f9a2
JailsonSantos <jailson.ads007@gmail.com>
2022-11-17 15:17:53 -0300

    CSS-in-JS - Criando Componente Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@components/Header';
+import { HeightLight } from '@components/Highlight';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { Container } from './styles';
@@ -7,6 +8,7 @@ export function Groups() {
   return (
     <Container>
       <Header />
+      <HeightLight title="Turmas" subtitle="Jogue com a sua Turma" />
     </Container>
   );
 }
