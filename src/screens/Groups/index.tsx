b892b21c2dec14340814e05832876917c1771559
JailsonSantos <jailson.ads007@gmail.com>
2022-11-17 16:07:37 -0300

    CSS-in-JS - Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { HeightLight } from '@components/Highlight';
 import { useState } from 'react';
 
 import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
@@ -25,6 +26,10 @@ export function Groups() {
           <GroupCard
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
 
