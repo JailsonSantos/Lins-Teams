410ffd6c31cf689ad8a798cb5d90622263b5c853
JailsonSantos <jailson.ads007@gmail.com>
2022-11-17 16:57:21 -0300

    CSS-in-JS - Criando Componente Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { HeightLight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -16,7 +17,9 @@ export function NewGroup() {
           subtitle="Cria a turma para adicioanr as pessoas."
         />
 
-        <Button title="Criar" />
+        <Input />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
 
       </Content>
     </Container>
