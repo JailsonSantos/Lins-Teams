a4bd95fa40491199641566a9496253661adea4b1
JailsonSantos <jailson.ads007@gmail.com>
2022-11-18 13:07:08 -0300

    Local Storage - Armazenando Players

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
-const GROUP_COLLECTION = '@lins-teams:groups'
+const GROUP_COLLECTION = '@lins-teams:groups';
+const PLAYER_COLLECTION = '@lins-teams:players';
 
-export { GROUP_COLLECTION };
+export { GROUP_COLLECTION, PLAYER_COLLECTION };
